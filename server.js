a80398763af832f63457b74d8f04508b275d26bb
Amal Amchtal <amchtala@union.edu>
2021-02-23 19:58:54 -0500

    fixed bugs?

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
  
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/messenger-app-frontend'));
+app.use(express.static(__dirname + '/messenger-app-frontend'));
  
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/dist/messenger-app-frontend/index.html'));
+  res.sendFile(path.join(__dirname + '/messenger-app-frontend/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
